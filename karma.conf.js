bd04fbcefe0108a433c3035d1d67b16b30ddaea5
sneha-me <snehanv24@gmail.com>
2018-01-03 13:14:34 +0530

    making systenjs to load files on the fly

--- karma.conf.js
+++ karma.conf.js
@@ -1,14 +1,15 @@
-module.exports = function(config) {
-  config.set({
-    basePath: '',
-    frameworks: ['jasmine'],
+    module.exports = function(config) {
+      config.set({
+        basePath: '',
+        frameworks: ['jasmine'],
 
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter')
-    ],
-	/**** Remember we said that included means that the file should added as a <script>. Well this is what’s causing the error. For tests, we don’t want our application files added as a <script>. We want SystemJS to load them, just like in the application. solets update files arrayas below  ****/
+        plugins: [
+          require('karma-jasmine'),
+          require('karma-chrome-launcher'),
+          require('karma-jasmine-html-reporter')
+        ],
+
+        /**** Remember we said that included means that the file should added as a <script>. Well this is what’s causing the error. For tests, we don’t want our application files added as a <script>. We want SystemJS to load them, just like in the application. solets update files arrayas below  ****/
           
         files: [
             // System.js for module loading
@@ -52,14 +53,13 @@ module.exports = function(config) {
 At this point, if we tried to run the test, nothing would happen, i.e. no tests would be run. This is because 1) The spec file is not added as a <script> and 2) we haven’t told SystemJS to load them yet.
 
 This is where we want to make use of a karma-test-shim.js file, that we can use just for tests. In this file, we will have SystemJS load all of our modules. ******************************************************************************************************************/
+        reporters: ['progress', 'kjhtml'],
 
-      reporters: ['progress', 'kjhtml'],
-
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
-  });
-};
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        browsers: ['Chrome'],
+        singleRun: false
+      });
+    };
